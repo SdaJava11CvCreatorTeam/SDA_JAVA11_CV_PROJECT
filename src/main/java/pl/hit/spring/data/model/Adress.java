755ba92d799d95866e2cd913b52a67f98c4b751e
Mariusz Guść <mariuszgusc@gmail.com>
2019-01-20 14:24:10 +0100

    Creation of the User class and special subclasses.

---
+++ src/main/java/pl/hit/spring/data/model/Adress.java
@@ -0,0 +1,8 @@
+package pl.hit.spring.data.model;
+
+public class Adress {
+
+    //  street houseNumber apartmentNumber                  Rzeźnicza 28/31
+    //  postCode City                                       50-130 Wrocław
+
+}
